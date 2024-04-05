d8febd856a7b06abc5d49ab63ce298df2977694a
IAMAmanRaj <imamanraj87@gmail.com>
2024-04-05 21:59:04 +0530

    minor changes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useLayoutEffect, useRef } from "react"
 import gsap from "gsap"
-
+import HeroSection from "./pages/HeroSection"
 const App = () => {
   const comp = useRef(null)
 
@@ -36,32 +36,27 @@ const App = () => {
     return () => ctx.revert()
   }, [])
   return (
-    <div className="relative" ref={comp}>
+    <div className="relative overflow-y-scroll overflow-x-hidden" ref={comp}>
       <div
         id="intro-slider"
-        className="h-screen p-10 bg-gray-50 absolute top-0 left-0 font-spaceGrotesk z-10 w-full flex flex-col gap-10 tracking-tight"
+        className="h-screen p-10 bg-gray-50 absolute top-0 left-0 font-spaceGrotesk z-10 w-full flex flex-col justify-around py-24 sm:py-8 pt-12   tracking-tight"
       >
-        <h1 className="text-9xl" id="title-1">
+        <h1 className="text-3xl  md:text-5xl lg:text-9xl" id="title-1">
           Hi there , I am Aman
         </h1>
-        <h1 className="text-9xl" id="title-2">
+        <h1 className="text-3xl  md:text-5xl lg:text-8xl" id="title-2">
         a Coding Enthusiast  
         </h1>
-        <h1 className="text-7xl" id="title-3">
+        <h1 className="text-3xl  md:text-5xl lg:text-7xl" id="title-3">
         Passionate about Web Development.
         </h1>
-        <h1 className="text-5xl" id="title-3">
+        <h1 className="text-3xl  md:text-5xl lg:text-6xl" id="title-3">
         and a Full stack Web Developer  :)
         </h1>
       </div>
-      <div className="h-screen flex bg-gray-950 justify-center place-items-center">
-        <h1
-          id="welcome"
-          className="text-9xl font-bold text-gray-100 font-spaceGrotesk"
-        >
-          Welcome.
-        </h1>
-      </div>
+    
+  <HeroSection/>
+      
     </div>
   )
 }
---
+++ app/src/pages/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className="h-screen flex bg-gray-950 justify-center place-items-center">
+        <h1 id="welcome"
+          className="text-3xl  md:text-5xl lg:text-9xl font-bold text-gray-100 font-spaceGrotesk"
+        >
+          Welcome.
+        </h1>
+      </div>
+  )
+}
+
+export default HeroSection
