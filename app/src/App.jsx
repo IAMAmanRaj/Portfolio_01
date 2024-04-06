9a44ecbb7c53afc31dfd6929fea389f734a53951
IAMAmanRaj <imamanraj87@gmail.com>
2024-04-06 20:03:25 +0530

    add animation changes to timeline

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,9 @@
 import { useLayoutEffect, useRef } from "react"
 import gsap from "gsap"
 import HeroSection from "./pages/HeroSection"
+import { useState } from "react"
 const App = () => {
+  const [journeyText, setJourneyText] = useState('My journey');
   const comp = useRef(null)
 
   useLayoutEffect(() => {
@@ -12,49 +14,69 @@ const App = () => {
         duration: 1.3,
         delay: 0.3,
       })
-        .from(["#title-1", "#title-2", "#title-3"], {
+        .from(["#title-1", "#title-2", "#title-3","#title-4"], {
           opacity: 0,
           y: "+=30",
-          stagger: 0.9,
+          x: "-=30",
+          stagger: 0.8,
+
         })
-        .to(["#title-1", "#title-2", "#title-3"], {
+        .to(["#title-1", "#title-2", "#title-3","#title-4"], {
           opacity: 0,
-          y: "-=30",
+       
+          x:"+=30",
           delay: 0.4,
-          stagger: 0.5,
+          stagger: 0.8,
         })
         .to("#intro-slider", {
           xPercent: "-100",
           duration: 1.3,
-        })
-        .from("#welcome", {
+         
+        }).from("#myjourney", {
           opacity: 0,
-          duration: 0.5,
+          duration: 2,
+          
+        }).from("#imageone", {
+          opacity: 0,
+          duration: 2,
+        }).from("#imagetwo", {
+          opacity: 0,
+          duration: 2,
+          
+          onStart: () => setJourneyText('Till'),
+          delay: 0.5,
+        }).from("#imagethree", {
+          opacity: 0,
+          duration: 2,
+          
+          onStart: () => setJourneyText('Now'),
+          delay: 0.5,
         })
+        
     }, comp)
 
     return () => ctx.revert()
   }, [])
   return (
-    <div className="relative  overflow-x-hidden overflow-y-hidden" ref={comp}>
+    <div className="relative  overflow-x-hidden" ref={comp}>
       <div
         id="intro-slider"
-        className="h-screen p-10 bg-zinc-600 absolute top-0 left-0 font-spaceGrotesk z-10 w-full flex flex-col justify-around py-24 sm:py-8 pt-12   tracking-tight"
+        className="h-screen p-10 bg-white absolute top-0 left-0 font-spaceGrotesk z-10 w-full flex flex-col justify-around py-24 sm:py-8 pt-12   tracking-tight"
       >
-        <h1 className="text-3xl text-slate-300  md:text-5xl lg:text-9xl" id="title-1">
+        <h1 className="text-3xl text-black  md:text-5xl lg:text-9xl" id="title-1">
           Hi there , I am Aman
         </h1>
-        <h1 className="text-3xl text-slate-300  md:text-5xl lg:text-8xl" id="title-2">
+        <h1 className="text-3xl text-black   md:text-5xl lg:text-8xl" id="title-2">
         a Coding Enthusiast  
         </h1>
-        <h1 className="text-3xl text-slate-300  md:text-5xl lg:text-7xl" id="title-3">
+        <h1 className="text-3xl text-black  md:text-5xl lg:text-7xl" id="title-3">
         Passionate about Web Development.
         </h1>
-        <h1 className="text-3xl text-slate-300  md:text-5xl lg:text-6xl" id="title-3">
+        <h1 className="text-3xl text-black  md:text-5xl lg:text-6xl" id="title-4">
         and a Full stack Web Developer  :)
         </h1>
       </div>
-    <div className="overflow-y-hidden"><HeroSection/></div>
+      <div className=""><HeroSection journeyText={journeyText}  /></div>
   
       
     </div>
--- app/src/pages/HeroSection.jsx
+++ app/src/pages/HeroSection.jsx
@@ -1,23 +1,59 @@
 import React, { useLayoutEffect } from 'react'
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import bulb from '/images/diego-ph-fIq0tET6llw-unsplash.jpg';
+import pages from '/images/brandi-redd-aJTiW00qqtI-unsplash.jpg'
+import comp from '/images/ben-kolde-bs2Ba7t69mM-unsplash.jpg';
 gsap.registerPlugin(ScrollTrigger) 
-const HeroSection = () => {
+const HeroSection = ({ journeyText }) => {
 
 useLayoutEffect(() => {
 
  gsap.to("#character", {
     opacity: 1,
-    stagger: 1,
+    stagger: 4.2,
     scrollTrigger: {
         trigger: "#character",
         scrub:true,
         markers:true,
-        start: "top 80%",
+        start: "top 88%",
         end: "top 20%",
     }
  })
 
+ gsap.to("#charactertwo", {
+    opacity: 1,
+    stagger: 1,
+    scrollTrigger: {
+        trigger: "#charactertwo",
+        scrub:true,
+        markers:true,
+        start: "top 90%",
+        end: "top 20%",
+    }
+ })
+ gsap.to("#characterthree", {
+    opacity: 1,
+    stagger: 1,
+    scrollTrigger: {
+        trigger: "#characterthree",
+        scrub:true,
+        markers:true,
+        start: "top 90%",
+        end: "top 20%",
+    }
+ })
+ gsap.to("#characterfour", {
+    opacity: 1,
+    stagger: 1,
+    scrollTrigger: {
+        trigger: "#characterfour",
+        scrub:true,
+        markers:true,
+        start: "top 90%",
+        end: "top 40%",
+    }
+ })
 
 }, [])
 
@@ -25,17 +61,26 @@ useLayoutEffect(() => {
 
   return (
     <div className="flex bg-gray-950 flex-col overflow-auto">
-        <div className="flex justify-center items-center h-screen">
-          <h1 id="welcome"
-            className="text-3xl  md:text-5xl lg:text-9xl font-bold text-gray-100 font-spaceGrotesk"
+        <div   className="relative flex justify-center items-center h-screen">
+          
+            
+
+            <img id="imageone" className='absolute h-screen rounded-full p-4 md:p-12 lg:p-16 xl:p-20 mx-12 mt-12   border-2 border-white w-full object-cover' src={bulb} alt=''/>
+            <img id="imagetwo" className='absolute h-screen rounded-full p-4 md:p-12 lg:p-16 xl:p-20 mx-12 mt-12   border-2 border-white w-full object-cover' src={pages} alt=''/>
+            <img id="imagethree" className='absolute h-screen rounded-full p-4 md:p-12  lg:p-16 xl:p-20 mx-12 mt-12   border-2 border-white w-full object-cover' src={comp} alt=''/>
+            <div id="myjourney" className='w-full absolute flex justify-center items-center  text-center self-center '>
+          <h1 
+          
+            className="text-3xl  absolute  md:text-5xl lg:text-8xl font-bold text-slate-100 font-spaceGrotesk transition duration-300"
           >
-            Welcome.
+            {journeyText}
           </h1>
+          </div>
         </div>
        
-        <div className='relative mx-auto text-xl  md:text-3xl lg:text-5xl  font-bold text-white '>
-         <p className='flex flex-wrap gap-x-3 px-8 '>
-            {"ScrollTrigger enables anyone to create jaw-dropping scroll-based animations with minimal code. Infinitely flexible. Scrub, pin, snap, or just trigger anything scroll-related, even if it has nothing to do with animation."
+        <div className='relative mx-auto text-xl  md:text-3xl lg:text-5xl  font-bold text-white mt-24'>
+         <p className='flex flex-wrap gap-x-3 px-8  '>
+            {"Few years back , I knew nothing about web. It got my attention in the second year of college when I started learning about languages and frameworks used to build websites like this and I was a-m-a-z-e-d. I knew this would be an interesting thing to do rather than doing dsa after classes T_T"
             .split(" ").map((word,index)=>(
                 <span id="character" key={index} className='opacity-0'>
                     {word.split("").map((character,index)=>(
@@ -47,18 +92,101 @@ useLayoutEffect(() => {
             ))}
             </p> 
             <p  className='absolute top-0 flex flex-wrap gap-x-3 opacity-5 px-8'>
-            {"ScrollTrigger enables anyone to create jaw-dropping scroll-based animations with minimal code. Infinitely flexible. Scrub, pin, snap, or just trigger anything scroll-related, even if it has nothing to do with animation."
+            {"Few years back , I knew nothing about web. It got my attention in the second year when I started learning about languages and frameworks used to build websites like this and I was a-m-a-z-e-d. I knew this would be an interesting thing to do rather than doing dsa after classes."
             .split(" ").map((word,index)=>(
-                <span id="word" key={index} className=''>
+                <span  key={index} className=''>
                     {word.split("").map((character,index)=>(
-                    <span id="character" key={index} className=''>
+                    <span  key={index} className=''>
                         {character}
                     </span>
                 ))} 
                 </span>
             ))}
             </p>
-        </div>
+            </div>
+            <div className='relative mx-auto text-xl  md:text-3xl lg:text-5xl  font-bold text-white mt-80'>
+            <p className='flex flex-wrap gap-x-3 px-8 '>
+            {"Then I started focusing on project based learning as much as I could. I've been making projects and learning things through this journey. As a developer, I feel there's no end to these cool stuffs which excites me even more :)"
+            .split(" ").map((word,index)=>(
+                <span id="charactertwo" key={index} className='opacity-0'>
+                    {word.split("").map((character,index)=>(
+                    <span id="charactertwo" key={index} className='opacity-0'>
+                        {character}
+                    </span>
+                ))} 
+                </span>
+            ))}
+            </p> 
+            
+            <p className='absolute top-0 flex flex-wrap gap-x-3 opacity-5 px-8'>
+            {"Then I started focusing on project based learning as much as I could. I've been making projects and learning things through this journey. As a developer, I feel there's no end to these cool stuffs which excites me even more :)"
+            .split(" ").map((word,index)=>(
+                <span  key={index} className=''>
+                    {word.split("").map((character,index)=>(
+                    <span  key={index} className=''>
+                        {character}
+                    </span>
+                ))} 
+                </span>
+            ))}
+            </p>
+            </div>
+
+            <div className='relative mx-auto text-xl  md:text-3xl lg:text-5xl  font-bold text-white mt-80'>
+            <p className='flex flex-wrap gap-x-3 px-8 '>
+            {"I've never stopped since then"
+            .split(" ").map((word,index)=>(
+                <span id="characterthree" key={index} className='opacity-0'>
+                    {word.split("").map((character,index)=>(
+                    <span id="characterthree" key={index} className='opacity-0'>
+                        {character}
+                    </span>
+                ))} 
+                </span>
+            ))}
+            </p> 
+            
+            <p className='absolute top-0 flex flex-wrap gap-x-3 opacity-5 px-8'>
+            {"I've never stopped since then"
+            .split(" ").map((word,index)=>(
+                <span  key={index} className=''>
+                    {word.split("").map((character,index)=>(
+                    <span  key={index} className=''>
+                        {character}
+                    </span>
+                ))} 
+                </span>
+            ))}
+            </p>
+            </div>
+            <div className='relative mx-auto text-xl  md:text-3xl lg:text-5xl  font-bold text-white mt-80'>
+            <p className='flex flex-wrap gap-x-3 px-8 '>
+            {"#Never stop learning."
+            .split(" ").map((word,index)=>(
+                <span id="characterfour" key={index} className='opacity-0'>
+                    {word.split("").map((character,index)=>(
+                    <span id="characterfour" key={index} className='opacity-0'>
+                        {character}
+                    </span>
+                ))} 
+                </span>
+            ))}
+            </p> 
+            
+            <p className='absolute top-0 flex flex-wrap gap-x-3 opacity-5 px-8'>
+            {"#Never stop learning."
+            .split(" ").map((word,index)=>(
+                <span  key={index} className=''>
+                    {word.split("").map((character,index)=>(
+                    <span  key={index} className=''>
+                        {character}
+                    </span>
+                ))} 
+                </span>
+            ))}
+            </p>
+            </div>
+        
         <div className='h-screen'/>
     </div>
   )
