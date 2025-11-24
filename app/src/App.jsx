f92de0abf485523ffb3ede1a8894468e92731227
IAMAmanRaj <imamanraj87@gmail.com>
2025-11-24 13:36:21 +0530

    refactor: remove unused Projects, Terms and Conditions, and Privacy Policy components

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,22 +1,15 @@
-import Projects from "./components/ui/Projects";
-
 import Landing from "./pages/Landing";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { ShiftingDropDown } from "./components/ui/ShiftingDropDown";
-import TermsandConditions from "./pages/TermsandConditions";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 const App = () => {
   return (
     <Router>
       <ShiftingDropDown />
       <Routes>
-        <Route path="/projects" element={<Projects />} />
         <Route path="/" element={<Landing />} />
-        <Route path="/terms&conditions" element={<TermsandConditions />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
       </Routes>
     </Router>
   );
