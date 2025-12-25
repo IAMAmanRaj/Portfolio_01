3eb71c7149717edd3d1d4766622c82240760ca2a
IAMAmanRaj <imamanraj87@gmail.com>
2025-12-25 15:48:36 +0530

    fix: update Projects Images

--- app/src/components/ui/Section.jsx
+++ app/src/components/ui/Section.jsx
@@ -34,7 +34,7 @@ export const HoverImageLinks = () => {
 
         <Link
           heading="Dice Game"
-          subheading="Incase you're bored , I got you covered with a Dice Game"
+          subheading="Incase you're bored, I got you covered with a Dice Game which I made long back !"
           imgSrc="/images/DiceGame.png"
           href="https://brilliant-bonbon-5d2ce1.netlify.app/"
         />
