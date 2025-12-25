1d1ad4f08168ef9b5d759f020a94ba84aecab406
IAMAmanRaj <imamanraj87@gmail.com>
2025-12-25 14:56:12 +0530

    fix: correct typos and improve clarity in Timeline component descriptions

--- app/src/components/ui/Timeline.jsx
+++ app/src/components/ui/Timeline.jsx
@@ -707,7 +707,7 @@ In March, I worked on our college’s Fest Website, which saw thousands of regis
     className="timeline-start pl-8 md:pl-0 text-current md:text-end pb-12 md:mr-3"
   >
     <time className="font-mono italic ">Summers, 2024</time>
-    <div className="text-lg font-black">Juggling, two interships and academics</div>In Summers, I was grateful to get an opportunity from <span className="bg-gradient-to-r from-[#812e2e] via-[20%] via-[#971313] to-[#e43535] bg-clip-text text-transparent font-bold text-3xl">Medino's</span> https://www.medinos.in , for the Full Stack Developer role. Worked on FE and BE of the MVP, that was involving a complex database schema. Got introduced to NestJS, Next.js, and tools like Prisma, Supabase to deal with SQL dbs. Here was the point when I started improving as a software developer. By making mistakes in production, and learning from them after panicking.</div>
+    <div className="text-lg font-black">Juggling, two internships and academics</div>In Summers, I was grateful to get an opportunity from <span className="bg-gradient-to-r from-[#812e2e] via-[20%] via-[#971313] to-[#e43535] bg-clip-text text-transparent font-bold text-3xl">Medino's</span> https://www.medinos.in , for the Full Stack Developer role. Worked on FE and BE of the MVP, that was involving a complex database schema. Got introduced to NestJS, Next.js, and tools like Prisma, Supabase to deal with SQL dbs. Here was the point when I started improving as a software developer. By making mistakes in production, and learning from them after panicking.</div>
   <hr ref={hrRef7} className="z-[-4]" />
 </li>
 <li>
@@ -734,7 +734,7 @@ In March, I worked on our college’s Fest Website, which saw thousands of regis
   and another one for its parent company <span className="hover:cursor-pointer text-blue-500 font-bold text-xl">SKDIV®</span>, 
   <a href="https://skdiv.com" className="text-blue-500 "> www.skdiv.com </a>. 
   Additionally, we completed a client project for an Australian business, delivering the <span className="bg-gradient-to-r from-[#8f5b27] via-[100%] via-[#bc8b04] to-[#f4a507] bg-clip-text text-transparent font-bold text-xl">True Cut Barber Studio</span>  website 
-  <a href="https://truecutbarberstudio.com.au" className="text-blue-500 hover:cursor-pointerx"> truecutbarberstudio.com.au </a>. Got introduced to the laws of UI/UX, metrics affecting page loading time, core web vitals and SEO. Before this, I used to think that UI mostly has to be visually appealing, but I was way too wrong. Complete mindset shift.
+  <a href="https://truecutbarberstudio.com.au" className="text-blue-500 hover:cursor-pointerx"> truecutbarberstudio.com.au </a>. Got introduced to the laws of UI/UX, metrics affecting page loading time, core web vitals and SEO. Before this, I used to think that UI mostly has to be visually appealing, but I was mistaken. Complete mindset shift.
 </div>
 <hr ref={hrRef8} className="z-[-4]" />
 </li>
@@ -782,7 +782,7 @@ October'24, I got the opportunity of leading the Web Team for Culrav-Avishkar, M
     className="timeline-end pl-8 text-current md:pl-0 mb-10 md:ml-3"
   >
     <time className="font-mono italic font-bold">Frontend Journey, to the ONE</time>
-            <div className="text-lg font-black"> </div> In Feb, I started working on<span className="hover:cursor-pointer bg-gradient-to-r from-[#4d01bf] via-[100%] via-[#4c018e] to-[#fefefe] bg-clip-text text-transparent font-bold text-3xl"> IRYS™</span> and worked on some challenging Frontend Tickets for the MVP. Lately been trying to become better at React.js and Javascript. Started working on some of my personal ideas. By this year end, my only goal is to become a better Frontend Engineer. Graduated in August with an offer in hand. Felt pretty good but i'm surely not there yet. Exploring the vastness of Frontend Engineering and trying to excel at what I do best. Period. </div>
+            <div className="text-lg font-black"> </div> In Feb, I started working on<span className="hover:cursor-pointer bg-gradient-to-r from-[#4d01bf] via-[100%] via-[#4c018e] to-[#fefefe] bg-clip-text text-transparent font-bold text-3xl"> IRYS™</span> and worked on some challenging Frontend Tickets for the MVP. Lately been trying to become better at React.js and Javascript. Started working on some of my personal ideas. By this year end, my only goal is to become a better Frontend Engineer. Graduated in August with an offer in hand. Felt good but i'm surely not there yet. Exploring the vastness of Frontend Engineering and trying to excel at what I do best. Period. </div>
  
 </li>
       </ul>
